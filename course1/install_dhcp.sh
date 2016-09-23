0b62396403acfd41dfacc19686c7eec1fa1a471f
Jeff W <jeffwji@yahoo.com>
2016-09-23 14:49:29 -0400

    update DHCP and DNS config

--- course1/install_dhcp.sh
+++ course1/install_dhcp.sh
@@ -12,16 +12,17 @@ yes | cp /usr/lib/systemd/system/dhcpd.service /etc/systemd/system/
 sed -i "s/\-\-no\-pid/$1/g" /etc/systemd/system/dhcpd.service
 
 echo Setup IP information...
-DHCP_IP_SELF=172.16.0.1
 
 SUBNET=172.16.0.0
 NET_MASK=255.255.255.0
 BROADCAST_ADDRESS=172.16.0.255
 DHCP_RANGE_FROM=172.16.0.150
 DHCP_RANGE_TO=172.16.0.250
-DHCP_OPTION_ROUTES=$DHCP_IP_SELF
-DHCP_DOMAIN_NAME_SERVERS="$DHCP_IP_SELF, 8.8.8.8"
+DHCP_DOMAIN_NAME_SERVERS=8.8.8.8
 DOMAIN_NAME=cloud.local.
+
+DHCP_IP_SELF=172.16.0.1
+DHCP_OPTION_ROUTES=$DHCP_IP_SELF
 
 MAC_ADDRESS=`ip link show $1 | awk '/link\/ether/{print $2}'`
 REV_SUBNET=$(echo $SUBNET | awk -F. '{for (i=NF-1; i>0; --i) printf "%s%s", $i, (i<NF-2 ? "" : ".")}')
@@ -29,31 +30,16 @@ REV_SUBNET=$(echo $SUBNET | awk -F. '{for (i=NF-1; i>0; --i) printf "%s%s", $i, 
 echo "
 default-lease-time 600;
 max-lease-time 7200;
-ddns-updates on;
-ddns-update-style interim;
-update-static-leases on;
 subnet $SUBNET netmask $NET_MASK {
     range $DHCP_RANGE_FROM $DHCP_RANGE_TO
     option routers $DHCP_OPTION_ROUTES;
     option domain-name-servers $DHCP_DOMAIN_NAME_SERVERS;
     option domain-name \"$DOMAIN_NAME\";
     option broadcast-address $BROADCAST_ADDRESS;
-    ddns-domainname \"$DOMAIN_NAME.\";
-    ddns-rev-domainname \"in-addr.arpa.\";
     host self {
         hardware ethernet $MAC_ADDRESS;
         fixed-address $DHCP_IP_SELF;
     }
-}
-allow unknown-clients;
-use-host-decl-names on;
-zone cloud.local. {
-    primary $DHCP_IP_SELF; # This server is the primary DNS server for the zone
-    key rndc-key;
-}
-zone $REV_SUBNET.in-addr.arpa. {
-    primary $DHCP_IP_SELF; # This server is the primary reverse DNS for the zone
-    key rndc-key;
 }" > /etc/dhcp/dhcpd.conf
 
 systemctl --system daemon-reload
